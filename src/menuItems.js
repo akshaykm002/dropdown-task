357e320c7c1a7d48b11ccf00a75442875c6b572d
akshaykm002 <akshaykm002@gmail.com>
2024-02-06 20:34:42 +0530

    first commit

---
+++ src/menuItems.js
@@ -0,0 +1,29 @@
+export const menuItems = [
+{
+    title: "Services",
+    submenu: [{
+            title: "web design",
+        },
+        {
+            title: "web development",
+            submenu: [{
+                    title: "Frontend",
+                },
+                {
+                    title: "Backend",
+                    submenu: [{
+                            title: "NodeJS",
+                        },
+                        {
+                            title: "PHP",
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            title: "SEO",
+        },
+    ],
+}
+];
